75ec0f25f995659f59c96029c5bc3f518d660f79
MTYBilly03 <53329783+MTYBilly03@users.noreply.github.com>
2019-09-16 19:32:52 -0600

    Create Hippo.java

---
+++ Hippo.java
@@ -0,0 +1,16 @@
+public class Hippo extends Pachyderm{
+		@Override
+		public void sleep()
+		{
+				System.out.println( "The Hippo is sleeps");
+		}
+		@Override
+		public void roam()
+		{
+				System.out.println( "The Hippo os roaming");
+		}
+		public void makeNoise()
+		{
+				System.out.println("The Hippo makes noise");
+		}
+}
