d50d660ccd4254544a4c18fdabd244c5590853a4
MTYBilly03 <53329783+MTYBilly03@users.noreply.github.com>
2019-09-16 19:32:32 -0600

    Create Dog.java

---
+++ Dog.java
@@ -0,0 +1,16 @@
+public class Dog extends Canine{
+		@Override
+		public void sleep()
+		{
+				System.out.println( "The Dog is sleeps");
+		}
+		@Override
+		public void roam()
+		{
+				System.out.println( "The Dog os roaming");
+		}
+		public void makeNoise()
+		{
+				System.out.println("The Dog makes noise");
+		}
+}
