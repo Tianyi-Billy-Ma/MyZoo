911c8d3a32c45f6cf084eba01bac87045945e018
MTYBilly03 <53329783+MTYBilly03@users.noreply.github.com>
2019-09-16 19:30:51 -0600

    Create Lion.java

---
+++ Lion.java
@@ -0,0 +1,16 @@
+public class Lion extends Feline{
+		@Override
+		public void sleep()
+		{
+				System.out.println("This Lion sleeps");
+		}
+		@Override
+		public void roam()
+		{
+				System.out.println("This Lion is roaming");
+		}
+		public void makeNoise()
+		{
+				System.out.println("The Lion makes noise");
+		}
+}
