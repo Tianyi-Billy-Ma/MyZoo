31783c3452bf4fcc860a3b325865f2f856daf9ed
MTYBilly03 <53329783+MTYBilly03@users.noreply.github.com>
2019-09-16 19:29:38 -0600

    Create Tiger.java

---
+++ Tiger.java
@@ -0,0 +1,16 @@
+public class Tigher extends Feline{
+		@Override
+		public void sleep()
+		{
+				System.out.println( "The tigher is sleeps");
+		}
+		@Override
+		public void roam()
+		{
+				System.out.println( "The tigher os roaming");
+		}
+		public void makeNoise()
+		{
+				System.out.println("The tiger makes noise");
+		}
+}
