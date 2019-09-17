a1cec5dcfc882a123ab1a21b6d42d2affeb3bccc
MTYBilly03 <53329783+MTYBilly03@users.noreply.github.com>
2019-09-16 19:30:06 -0600

    Create Cat.java

---
+++ Cat.java
@@ -0,0 +1,16 @@
+public class Cat extends Feline{
+		@Override
+		public void sleep()
+		{
+				System.out.println("This Cat sleeps");
+		}
+		@Override
+		public void roam()
+		{
+				System.out.println("This Cat is roaming");
+		}
+		public void makeNoise()
+		{
+				System.out.println("The Cat makes noise");
+		}
+}
