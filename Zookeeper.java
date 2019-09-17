3c42aa456f6b443e0435626213513ef2390ce2c9
MTYBilly03 <53329783+MTYBilly03@users.noreply.github.com>
2019-09-16 19:34:11 -0600

    Create Zookeeper.java

---
+++ Zookeeper.java
@@ -0,0 +1,7 @@
+public class Zookeeper{
+	public static void main(String[] args){
+		System.out.println("Hi, I'm Tianyi.");
+		System.out.println("I'm the zookeeper.");
+		System.out.println(args);
+	}
+}
