a4d69d570a1c3fc063dc3810f58bc0d60617f674
MTYBilly03 <53329783+MTYBilly03@users.noreply.github.com>
2019-09-16 19:33:21 -0600

    Create Elephant.java

---
+++ Elephant.java
@@ -0,0 +1,16 @@
+public class Elephant extends Pachyderm{
+		@Override
+		public void sleep()
+		{
+				System.out.println( "The Elephant is sleeps");
+		}
+		@Override
+		public void roam()
+		{
+				System.out.println( "The Elephant os roaming");
+		}
+		public void makeNoise()
+		{
+				System.out.println("The Elephant makes noise");
+		}
+}
