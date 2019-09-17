b15fef083f4b5eb31d02e6c95180986d0eec7198
MTYBilly03 <53329783+MTYBilly03@users.noreply.github.com>
2019-09-16 19:33:44 -0600

    Create Rhino.java

---
+++ Rhino.java
@@ -0,0 +1,16 @@
+public class Rhino extends Pachyderm{
+		@Override
+		public void sleep()
+		{
+				System.out.println( "The Rhino is sleeps");
+		}
+		@Override
+		public void roam()
+		{
+				System.out.println( "The Rhino os roaming");
+		}
+		public void makeNoise()
+		{
+				System.out.println("The Rhino makes noise");
+		}
+}
