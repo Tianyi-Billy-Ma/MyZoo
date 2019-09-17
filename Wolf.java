840392b6cc246db9523a27edc815da614037262c
MTYBilly03 <53329783+MTYBilly03@users.noreply.github.com>
2019-09-16 19:31:47 -0600

    Create Wolf.java

---
+++ Wolf.java
@@ -0,0 +1,16 @@
+public class Wolf extends Canine{
+		@Override
+		public void sleep()
+		{
+				System.out.println( "The Wolf is sleeps");
+		}
+		@Override
+		public void roam()
+		{
+				System.out.println( "The Wolf os roaming");
+		}
+		public void makeNoise()
+		{
+				System.out.println("The Wolf makes noise");
+		}
+}
