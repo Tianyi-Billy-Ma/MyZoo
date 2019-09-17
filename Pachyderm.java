008395c29af0364a6d3af40371b662fa6863e26d
MTYBilly03 <53329783+MTYBilly03@users.noreply.github.com>
2019-09-16 19:27:34 -0600

    Create Pachyderm.java

---
+++ Pachyderm.java
@@ -0,0 +1,11 @@
+public abstract class Pachyderm extends Animal{
+		public void roam()
+		{
+			System.out.println("This Pachyderm is roaming");
+		}
+		@Override
+		public void sleep()
+		{
+				System.out.println("This Pachyderm sleeps");
+		}
+}
