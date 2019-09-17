8f5dbc43e63cf73cca5ad576523600b88805843e
MTYBilly03 <53329783+MTYBilly03@users.noreply.github.com>
2019-09-16 19:26:42 -0600

    Create Canine.java

---
+++ Canine.java
@@ -0,0 +1,11 @@
+public abstract class Canine extends Animal{
+		public void roam()
+		{
+			System.out.println("This Canine is roaming");
+		}
+		@Override
+		public void sleep()
+		{
+				System.out.println("This Canine sleeps");
+		}
+}
