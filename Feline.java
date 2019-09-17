5c39f7dc12c76d88304b2df335ed14765695aec6
MTYBilly03 <53329783+MTYBilly03@users.noreply.github.com>
2019-09-16 19:28:41 -0600

    Create Feline.java

---
+++ Feline.java
@@ -0,0 +1,11 @@
+public abstract class Feline extends Animal{
+		public void roam()
+		{
+			System.out.println("This Feline is roaming");
+		}
+		@Override
+		public void sleep()
+		{
+				System.out.println("This Feline sleeps");
+		}
+}
